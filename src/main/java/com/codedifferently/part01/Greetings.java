4f89c0c73e66eb9a3d8dceb2c47d1957025f65bf
samghebre <samuelka2001@yahoo.com>
2020-10-26 05:27:08 +0000

    Commit part 1

--- src/main/java/com/codedifferently/part01/Greetings.java
+++ src/main/java/com/codedifferently/part01/Greetings.java
@@ -1,7 +1,18 @@
 package com.codedifferently.part01;
 
+import java.util.Scanner;
+
 public class Greetings {
     public static void main(String[] args) {
+        System.out.println("What is your name? ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
 
+        if(name.equals("Hakeem") || name.equals("Rasheeda")) {
+            System.out.println("Hello, " + name);
+        }
+        else {
+            System.out.println("Sorry! I didn't recognize your name. ");
+        }
     }
 }
