93c6d3b9e58fec9d4925c75aaa3e7ee98c74cca8
samghebre <samuelka2001@yahoo.com>
2020-10-27 17:36:19 +0000

    commit three

--- src/main/java/com/codedifferently/part02/TooHighTooLow.java
+++ src/main/java/com/codedifferently/part02/TooHighTooLow.java
@@ -1,16 +1,30 @@
 package com.codedifferently.part02;
-
+import java.util.Scanner;
 public class TooHighTooLow {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int counter = 0;
         int mystryNum = 7;
-        System.out.println("Gues a number between 1 qnd 10");
-        int guesNum = 
-    
+        System.out.println("Gues a number between 1 and 10");
+        int guesNum = input.nextInt();
+        while (guesNum != mystryNum) {
+            if (guesNum > mystryNum) {
+                System.out.println("Number is too high"); 
+                counter += 1;   
+                System.out.println("Gues a number between 1 and 10");
+                guesNum = input.nextInt(); 
+            }
+            else if (guesNum < mystryNum) {
+                System.out.println("Number is too low");
+                counter += 1;   
+                System.out.println("Gues a number between 1 and 10");
+                guesNum = input.nextInt(); 
+            }
+             
+            
+        }
+        counter +=1;
+        System.out.println("you hit the target number!  " + counter);
     }
-
-
-
 }
