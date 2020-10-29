a168ba563fae25497febd64aaff56b72c8ab1383
samghebre <samuelka2001@yahoo.com>
2020-10-29 02:19:35 +0000

    Completed week 01 part 02 Assignment

--- src/main/java/com/codedifferently/part02/TooHighTooLow.java
+++ src/main/java/com/codedifferently/part02/TooHighTooLow.java
@@ -5,7 +5,7 @@ public class TooHighTooLow {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int counter = 0;
-        int mystryNum = 7;
+        int mystryNum = 5;
         System.out.println("Gues a number between 1 and 10");
         int guesNum = input.nextInt();
         while (guesNum != mystryNum) {
